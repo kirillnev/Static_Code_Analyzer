dfa58e4d3ba1671adf7eb21149895b713269a907
Kirill Nevinskii <mail@kirillnev.ru>
2023-02-02 09:12:40 +0600

    Stage_2_5

--- Static Code Analyzer/task/analyzer/code_analyzer.py
+++ Static Code Analyzer/task/analyzer/code_analyzer.py
@@ -1,22 +1,84 @@
 class PEP8:
     MAX_LINE_LEN = 79
-    msg = {
-        'S001': 'Too long'
-    }
+
 
     def __init__(self, lines):
-        self.issues = {}
+        self.line_issues = {}
         self.lines = lines
+        self.CHECKS = {
+            'S001': {
+                'func': self.check_s001,
+                'msg': 'Too long'
+            },
+            'S002': {
+                'func': self.check_s002,
+                'msg': 'Indentation is not a multiple of four'
+            },
+            'S003': {
+                'func': self.check_s003,
+                'msg': 'Unnecessary semicolon'
+            },
+            'S004': {
+                'func': self.check_s004,
+                'msg': 'At least two spaces required before inline comments'
+            },
+            'S005': {
+                'func': self.check_s005,
+                'msg': 'TODO found'
+            },
+            'S006': {
+                'func': None,
+                'msg': 'More than two blank lines preceding a code line'
+            }
+        }
 
-    def check_lines_len(self):
-        for i in range(len(self.lines)):
-            if len(self.lines[i]) > self.MAX_LINE_LEN:
-                self. issues[i] = 'S001'
+    def check_s001(self, line):
+        return len(line) <= self.MAX_LINE_LEN
+
+    def check_s002(self, line):
+        return len(line.lstrip()) == 0 or (len(line) - len(line.lstrip())) % 4 == 0
+
+    def check_s003(self, line):
+        return line.split('#')[0].rstrip()[::-1].find(';') != 0
+
+    def check_s004(self, line):
+        return line.strip().find('#') <= 0 or line.strip().find('  #') > 0
+
+    def check_s005(self, line):
+        return len(line.split('#')) == 1 or line.split('#')[-1].lower().find('todo') == -1
+
+    def check_s006(self, line):
+        return True
+
+    def check_line(self, line):
+        issues = []
+        for key, value in self.CHECKS.items():
+            if value['func'] is not None:
+                if not value['func'](line):
+                    issues.append(key)
+        return issues
+
+    def check_lines(self):
+        empty_lines = 0
+        for i, line in enumerate(self.lines):
+            if len(line.strip()) == 0:
+                empty_lines += 1
+            elif len(self.check_line(line)) > 0:
+                self.line_issues[i] = self.check_line(line)
+                empty_lines = 0
+
+            if empty_lines > 2 and len(line.strip()) > 0:
+                if self.line_issues.get(i) is not None:
+                    self.line_issues[i] += ['S006']
+                else:
+                    self.line_issues[i] = ['S006']
+                empty_lines = 0
 
     def __str__(self):
         result = ''
-        for line_num, issue in self.issues.items():
-            result += f'Line {line_num + 1}: {issue} {self.msg[issue]}\n'
+        for line_num, issues in self.line_issues.items():
+            for issue in issues:
+                result += f'Line {line_num + 1}: {issue} {self.CHECKS[issue]["msg"]}\n'
         return result
 
 
@@ -32,14 +94,14 @@ class StaticCodeAnalyzer:
 
     def run(self):
         pep8 = PEP8(self.read_file())
-        pep8.check_lines_len()
+        pep8.check_lines()
         if len(str(pep8)) > 0:
             print(pep8)
 
 
 if __name__ == '__main__':
     file_name = input()
-    # file_name = '../test/single_line_valid_example.py'
+    # file_name = 'my_test.py'
 
     sca = StaticCodeAnalyzer(file_name)
     sca.run()
