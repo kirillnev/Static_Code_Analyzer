727b42da867a9cd95f84fdc5c26727f5c33a0e16
Kirill Nevinskii <mail@kirillnev.ru>
2023-02-07 18:09:22 +0600

    Stage_5_5

--- Static Code Analyzer/task/analyzer/code_analyzer.py
+++ Static Code Analyzer/task/analyzer/code_analyzer.py
@@ -1,3 +1,4 @@
+import ast
 import os
 import re
 import sys
@@ -7,8 +8,9 @@ class PEP8:
     MAX_LINE_LEN = 79
 
     def __init__(self, lines):
-        self.line_issues = {}
+        self.line_issues = []
         self.lines = lines
+        self.ast_tree = ast.parse(''.join(lines))
         self.CHECKS = {
             'S001': {
                 'func': self.check_s001,
@@ -42,10 +44,22 @@ class PEP8:
                 'func': self.check_s008,
                 'msg': 'Class name class_name should be written in CamelCase'
             },
-            'S009': {
-                'func': self.check_s009,
-                'msg': 'Function name function_name should be written in snake_case'
-            },
+            # 'S009': {
+            #     'func': self.check_s009,
+            #     'msg': 'Function name function_name should be written in snake_case'
+            # },
+            # 'S010': {
+            #     'func': self.check_s010,
+            #     'msg': 'Argument name arg_name should be written in snake_case'
+            # },
+            # 'S011': {
+            #     'func': self.check_s011,
+            #     'msg': 'Variable var_name should be written in snake_case'
+            # },
+            # 'S012': {
+            #     'func': self.check_s012,
+            #     'msg': 'The default argument value is mutable'
+            # },
         }
 
     def check_s001(self, line_num):
@@ -80,40 +94,69 @@ class PEP8:
         regex_tmpl = r' *(?:class|def) {2,}'
         return re.match(regex_tmpl, line) is None
 
+    def is_snake_case(self, name):
+        template = r'\b[_a-z0-9]+\b'
+        return re.match(template, name) is not None
+
     def check_s008(self, line_num):
         line = self.lines[line_num]
         regex_tmpl = r'class +[a-z]'
         return re.match(regex_tmpl, line) is None
 
-    def check_s009(self, line_num):
-        line = self.lines[line_num]
-        regex_tmpl = r'def +_*[A-Z]'
-        return re.match(regex_tmpl, line) is None
+    def check_s009(self):
+        for node in ast.walk(self.ast_tree):
+            if isinstance(node, ast.FunctionDef):
+                function_name = node.name
+                line_num = node.lineno
+                key = 'S009'
+                if not self.is_snake_case(function_name):
+                    self.line_issues.append({'line': line_num, 'code': key,
+                                             'msg': f"Function name '{function_name}' should use snake_case"})
+
+
+    def check_s010(self):
+        for node in ast.walk(self.ast_tree):
+            if isinstance(node, ast.FunctionDef):
+                line_num = node.lineno
+                key = 'S010'
+                for arg in node.args.args:
+                    if not self.is_snake_case(arg.arg):
+                        self.line_issues.append({'line': line_num, 'code': key,
+                                                 'msg': f"Argument name '{arg.arg}' should be written in snake_case"})
+
+    def check_s011(self):
+        for node in ast.walk(self.ast_tree):
+            if isinstance(node, ast.FunctionDef):
+                line_num = node.lineno
+                key = 'S011'
+                for node_in_def in node.body:
+                    if isinstance(node_in_def, ast.Assign):
+                        for target in node_in_def.targets:
+                            if hasattr(target, "id"):
+                                variable_name = target.id
+                                if not self.is_snake_case(variable_name):
+                                    self.line_issues.append({'line': line_num + 1, 'code': key,
+                                                             'msg': f"Variable '{variable_name}' in function should be snake_case"})
+
+    def check_s012(self):
+        for node in ast.walk(self.ast_tree):
+            if isinstance(node, ast.FunctionDef):
+                line_num = node.lineno
+                key = 'S012'
+                for default in node.args.defaults:
+                    if not isinstance(default, ast.Constant):
+                        self.line_issues.append({'line': line_num, 'code': key,
+                                                 'msg': "Default argument value is mutable"})
 
     def check_line(self, line_num):
-        issues = []
         if len(self.lines[line_num].strip()) > 0:
             for key, value in self.CHECKS.items():
                 if not value['func'](line_num):
-                    issues.append(key)
-        return issues
+                    self.line_issues.append({'line': line_num + 1, 'code': key, 'msg': self.CHECKS[key]["msg"]})
 
     def check_lines(self):
-        self.line_issues = {i: self.check_line(i) for i in range(len(self.lines))}
-
-    def check_result(self):
-        result = []
-        for line_num, issues in self.line_issues.items():
-            for issue in issues:
-                result.append({'line': line_num + 1, 'code': issue, 'msg': self.CHECKS[issue]["msg"]})
-        return result
-
-    def __str__(self):
-        result = ''
-        for line_num, issues in self.line_issues.items():
-            for issue in issues:
-                result += f'Line {line_num + 1}: {issue} {self.CHECKS[issue]["msg"]}\n'
-        return result
+        for i in range(len(self.lines)):
+            self.check_line(i)
 
 
 class StaticCodeAnalyzer:
@@ -129,7 +172,11 @@ class StaticCodeAnalyzer:
     def run(self):
         pep8 = PEP8(self.read_file())
         pep8.check_lines()
-        result = pep8.check_result()
+        pep8.check_s009()
+        pep8.check_s010()
+        pep8.check_s011()
+        pep8.check_s012()
+        result = pep8.line_issues
         if len(result) > 0:
             for r in result:
                 print(f'{self.file_path}: Line {r["line"]}: {r["code"]} {r["msg"]}')
